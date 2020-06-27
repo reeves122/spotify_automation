fa140ef1c77942f048e4f55457910a8123b54a2d
Justin <reeves122@gmail.com>
2020-06-27 13:59:58 -0500

    Added update_local_cache, create_track_hash, load_all_disliked_tracks, scan_playlist_for_disliked_tracks, process_queue_playlist

--- spotify_automation/spotify_automation.py
+++ spotify_automation/spotify_automation.py
@@ -7,7 +7,7 @@ from spotipy import SpotifyOAuth, Spotify
 
 logging.getLogger().setLevel('INFO')
 USERNAME = os.environ.get('USERNAME')
-CACHE_DIR = os.environ.get('spotify_cache')
+CACHE_DIR = os.environ.get('CACHE_DIR', '.spotify_cache')
 MAX_PLAYLIST_TRACKS = 11000
 
 
@@ -28,7 +28,7 @@ def login() -> Spotify:
             'user-library-modify ' \
             'user-read-recently-played'
 
-    session = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    session = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, username=USERNAME))
     logging.info(f"Successfully logged in as: {USERNAME}")
     return session
 
@@ -44,7 +44,7 @@ def get_all_playlists(session: Spotify) -> list:
     all_playlists = []
 
     for playlist_offset in range(0, 10000, 50):
-        playlists = session.user_playlists(USERNAME, limit=50, offset=playlist_offset)
+        playlists = session.user_playlists(user=USERNAME, limit=50, offset=playlist_offset)
 
         if len(playlists['items']) < 1:
             break
@@ -67,7 +67,8 @@ def get_playlist_tracks(session: Spotify, playlist_id: str) -> list:
     tracks_in_playlist = []
     for track_offset in range(0, MAX_PLAYLIST_TRACKS, 100):
 
-        results = session.playlist_tracks(USERNAME, playlist_id, limit=100, offset=track_offset)
+        results = session.user_playlist_tracks(USERNAME, playlist_id,
+                                               limit=100, offset=track_offset)
         if len(results['items']) < 1:
             break
 
@@ -96,8 +97,8 @@ def save_tracks_file(playlist_name, playlist_tracks) -> None:
     """
     Write a list of playlist tracks to a json file
 
-    :param playlist_name: Name of playlist to save
-    :param playlist_tracks: List of track dictionaries
+    :param playlist_name:       Name of playlist to save
+    :param playlist_tracks:     List of track dictionaries
     """
     file_name = os.path.join(CACHE_DIR, playlist_name + '.json')
     logging.info('Saving playlists tracks to file: "{}"'.format(file_name))
@@ -105,3 +106,107 @@ def save_tracks_file(playlist_name, playlist_tracks) -> None:
         file_handle.write(json.dumps(playlist_tracks, indent=4, default=str))
 
     logging.info(f'Saved {len(playlist_tracks)} tracks to {file_name}')
+
+
+def update_local_cache(session: Spotify, all_playlists: list) -> None:
+    """
+    Compare the local track cache files with Spotify and update the local cache as necessary.
+    For lack of a better option, the size comparison of the playlist is used to detect changes.
+
+    :param session:             Spotipy session
+    :param all_playlists        List of playlist definitions
+    """
+    logging.info('Updating local cache of playlists...')
+    for playlist in all_playlists:
+        cache_tracks = load_tracks_file(playlist['name'])
+
+        if len(cache_tracks) == playlist['tracks']['total']:
+            continue
+
+        logging.info(f"Detected changes in playlist {playlist['name']}'. Updating local cache.")
+
+        save_tracks_file(playlist['name'], get_playlist_tracks(session, playlist['id']))
+
+
+def create_track_hash(track_list: list) -> dict:
+    """
+    Create a dict hash map of tracks by track id to enable fast lookups
+
+    :param track_list:      List of track dictionary items
+    """
+    return {track['id']: track for track in track_list}
+
+
+def load_all_disliked_tracks(playlists: list) -> dict:
+    """
+    Load all the tracks from all the "disliked" playlists to create a single, large list
+
+    :param playlists        List of playlist definitions
+    """
+    logging.info('Loading disliked tracks...')
+
+    # Load all disliked tracks into a list
+    disliked_tracks = []
+    [disliked_tracks.extend(load_tracks_file(playlist['name']))
+     for playlist in playlists if playlist['name'].startswith('disliked_')]
+
+    logging.info('Loaded {} disliked tracks'.format(len(disliked_tracks)))
+
+    # Convert list to a hash map by track ID
+    return create_track_hash(disliked_tracks)
+
+
+def scan_playlist_for_disliked_tracks(session: Spotify, playlist: dict,
+                                      disliked_tracks_hash: dict) -> None:
+    """
+    Scan the specified playlist for tracks which are in the disliked list and remove them
+    from the playlist
+
+    :param session:                     Spotipy session
+    :param playlist:                    Playlist definition which will be loaded to scan
+    :param disliked_tracks_hash:        Dict hash of disliked tracks
+    """
+    if playlist['name'].startswith('disliked_'):
+        return
+
+    logging.debug('Scanning for disliked tracks in playlist "{}"'.format(playlist['name']))
+
+    for track in load_tracks_file(playlist['name']):
+
+        if disliked_tracks_hash.get(track['id']):
+
+            logging.info('Disliked track found: Artist:"{}" Name:"{}" URI:"{}"'.format(
+                track['artists'][0]['name'], track['name'], track['uri']))
+
+            session.user_playlist_remove_all_occurrences_of_tracks(
+                USERNAME, playlist['id'], [track['id']])
+
+
+def process_queue_playlist(session, playlist):
+    """
+    Scan a "Queue" playlist (playlist of songs yet to be listened to and rated) for songs
+    which have been added to the corresponding non-queue playlist. For example, the user may
+    have "Favorites" and "Favorites Queue" playlists. The latter being songs the user has not
+    heard and rated before. If the user likes a song, they add it to the "Favorites" list and this
+    function will then remove it from the "Favorites Queue" playlist.
+
+    :param session:                     Spotipy session
+    :param playlist:                    Playlist definition which will be loaded to scan
+    """
+    if not playlist['name'].endswith(' Queue'):
+        return
+
+    logging.debug('Scanning queue playlist "{}"'.format(playlist['name']))
+
+    destination_playlist_track_hash = create_track_hash(
+        load_tracks_file(playlist['name'].replace(' Queue', '')))
+
+    for track in load_tracks_file(playlist['name']):
+
+        if destination_playlist_track_hash.get(track['id']):
+
+            logging.info('Track found in destination: Artist:"{}" Name:"{}" URI:"{}"'.format(
+                track['artists'][0]['name'], track['name'], track['uri']))
+
+            session.user_playlist_remove_all_occurrences_of_tracks(
+                USERNAME, playlist['id'], [track['id']])
--- tests/test_spotify_automation.py
+++ tests/test_spotify_automation.py
@@ -49,7 +49,7 @@ class TestSpotifyAutomation(unittest.TestCase):
         call_1_results = [{'track': {}} for _ in range(50)]
         call_2_results = [{'track': {}} for _ in range(10)]
 
-        session.playlist_tracks.side_effect = [
+        session.user_playlist_tracks.side_effect = [
             {'items': call_1_results},
             {'items': call_2_results},
             {'items': []}
@@ -72,3 +72,119 @@ class TestSpotifyAutomation(unittest.TestCase):
         util.save_tracks_file('test_file', [{}, {}, {}, {}, {}])
         results = util.load_tracks_file('test_file')
         self.assertEqual(5, len(results))
+
+    def test_update_local_cache(self):
+        """
+        Test updating the local cache using the test file
+        """
+        session = Mock()
+        playlists = [
+            {
+                'name': 'test_file',
+                'id': '12345',
+                'tracks': {'total': 100}
+            }
+        ]
+
+        call_1_results = [{'track': {}} for _ in range(50)]
+        call_2_results = [{'track': {}} for _ in range(20)]
+
+        session.user_playlist_tracks.side_effect = [
+            {'items': call_1_results},
+            {'items': call_2_results},
+            {'items': []}
+        ]
+        util.update_local_cache(session, playlists)
+        results = util.load_tracks_file('test_file')
+        self.assertEqual(70, len(results))
+
+    def test_create_track_hash(self):
+        """
+        Test creating a hash map of tracks
+        """
+        result = util.create_track_hash([{'id': '123'}, {'id': '456'}])
+        self.assertEqual({
+            '123': {'id': '123'},
+            '456': {'id': '456'}
+        }, result)
+
+    def test_load_all_disliked_tracks(self):
+        """
+        Test loading a list of "disliked" tracks
+        """
+        util.save_tracks_file('disliked_14', [{'id': '123'}, {'id': '456'}, {'id': '789'}])
+        util.save_tracks_file('disliked_23', [{'id': '987'}, {'id': '654'}, {'id': '321'}])
+        playlists = [
+            {'name': 'disliked_14'},
+            {'name': 'disliked_23'}
+        ]
+        results = util.load_all_disliked_tracks(playlists)
+        os.remove('disliked_14.json')
+        os.remove('disliked_23.json')
+        self.assertEqual({
+            '123': {'id': '123'},
+            '456': {'id': '456'},
+            '789': {'id': '789'},
+            '987': {'id': '987'},
+            '654': {'id': '654'},
+            '321': {'id': '321'}
+        }, results)
+
+    def test_scan_playlist_for_disliked_tracks(self):
+        """
+        Test removing 2 disliked tracks from the test playlist. Track ID 789 should remain.
+        """
+        session = Mock()
+        session.user_playlist_remove_all_occurrences_of_tracks.return_value = None
+
+        disliked = {
+            '123': {'id': '123'},
+            '456': {'id': '456'}
+        }
+
+        playlist = {
+            'name': 'Foo Playlist',
+            'id': '123456789'
+        }
+
+        playlist_tracks = [
+            {'id': '123', 'name': 'Track1', 'uri': '1', 'artists': [{'name': 'Foo Artist 1'}]},
+            {'id': '456', 'name': 'Track2', 'uri': '1', 'artists': [{'name': 'Foo Artist 2'}]},
+            {'id': '789', 'name': 'Track3', 'uri': '1', 'artists': [{'name': 'Foo Artist 3'}]},
+        ]
+
+        util.save_tracks_file('Foo Playlist', playlist_tracks)
+
+        util.scan_playlist_for_disliked_tracks(session, playlist, disliked)
+        os.remove('Foo Playlist.json')
+        self.assertEqual(2, session.user_playlist_remove_all_occurrences_of_tracks.call_count)
+
+    def test_process_queue_playlist(self):
+        """
+        Test removing 2 disliked tracks from the test playlist. Track ID 789 should remain.
+        """
+        session = Mock()
+        session.user_playlist_remove_all_occurrences_of_tracks.return_value = None
+
+        queue_playlist = {
+            'name': 'Foo Playlist Queue',
+            'id': '123456789'
+        }
+
+        queue_playlist_tracks = [
+            {'id': '123', 'name': 'Track1', 'uri': '1', 'artists': [{'name': 'Foo Artist 1'}]},
+            {'id': '456', 'name': 'Track2', 'uri': '1', 'artists': [{'name': 'Foo Artist 2'}]},
+            {'id': '789', 'name': 'Track3', 'uri': '1', 'artists': [{'name': 'Foo Artist 3'}]},
+        ]
+        util.save_tracks_file('Foo Playlist Queue', queue_playlist_tracks)
+
+        destination_playlist_tracks = [
+            {'id': '456', 'name': 'Track2', 'uri': '1', 'artists': [{'name': 'Foo Artist 2'}]},
+            {'id': '789', 'name': 'Track3', 'uri': '1', 'artists': [{'name': 'Foo Artist 3'}]},
+        ]
+        util.save_tracks_file('Foo Playlist', destination_playlist_tracks)
+
+        util.process_queue_playlist(session, queue_playlist)
+        os.remove('Foo Playlist Queue.json')
+        os.remove('Foo Playlist.json')
+        self.assertEqual(2, session.user_playlist_remove_all_occurrences_of_tracks.call_count)
