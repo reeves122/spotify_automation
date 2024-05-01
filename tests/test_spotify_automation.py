91b286a2571b67df8a86e91cdfd3327d5e0f2fad
Justin Reeves <reeves122@gmail.com>
2024-05-01 11:05:51 -0500

    Remove disliked tracks tests

--- tests/test_spotify_automation.py
+++ tests/test_spotify_automation.py
@@ -108,57 +108,6 @@ class TestSpotifyAutomation(unittest.TestCase):
             '456': {'id': '456'}
         }, result)
 
-    def test_load_all_disliked_tracks(self):
-        """
-        Test loading a list of "disliked" tracks
-        """
-        util.save_tracks_file('disliked_14', [{'id': '123'}, {'id': '456'}, {'id': '789'}])
-        util.save_tracks_file('disliked_23', [{'id': '987'}, {'id': '654'}, {'id': '321'}])
-        playlists = [
-            {'name': 'disliked_14'},
-            {'name': 'disliked_23'}
-        ]
-        results = util.load_all_disliked_tracks(playlists)
-        os.remove('disliked_14.json')
-        os.remove('disliked_23.json')
-        self.assertEqual({
-            '123': {'id': '123'},
-            '456': {'id': '456'},
-            '789': {'id': '789'},
-            '987': {'id': '987'},
-            '654': {'id': '654'},
-            '321': {'id': '321'}
-        }, results)
-
-    def test_scan_playlist_for_disliked_tracks(self):
-        """
-        Test removing 2 disliked tracks from the test playlist. Track ID 789 should remain.
-        """
-        session = Mock()
-        session.user_playlist_remove_all_occurrences_of_tracks.return_value = None
-
-        disliked = {
-            '123': {'id': '123'},
-            '456': {'id': '456'}
-        }
-
-        playlist = {
-            'name': 'Foo Playlist',
-            'id': '123456789'
-        }
-
-        playlist_tracks = [
-            {'id': '123', 'name': 'Track1', 'uri': '1', 'artists': [{'name': 'Foo Artist 1'}]},
-            {'id': '456', 'name': 'Track2', 'uri': '1', 'artists': [{'name': 'Foo Artist 2'}]},
-            {'id': '789', 'name': 'Track3', 'uri': '1', 'artists': [{'name': 'Foo Artist 3'}]},
-        ]
-
-        util.save_tracks_file('Foo Playlist', playlist_tracks)
-
-        util.scan_playlist_for_disliked_tracks(session, playlist, disliked)
-        os.remove('Foo Playlist.json')
-        self.assertEqual(2, session.user_playlist_remove_all_occurrences_of_tracks.call_count)
-
     def test_process_queue_playlist(self):
         """
         Test removing 2 disliked tracks from the test playlist. Track ID 789 should remain.
